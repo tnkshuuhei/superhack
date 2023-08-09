a9def3beb024d9bfd9acac4e75c2209b56001ba2
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-08-09 18:26:57 +0900

    delete lib

--- lib/apolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const apolloClient = new ApolloClient({
-  uri: "https://sepolia.easscan.org/graphql",
-  cache: new InMemoryCache(),
-});
-
-export default apolloClient;

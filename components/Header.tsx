7b19125cf7c564046a655c3cc1be507d091e0295
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-08-11 22:11:50 +0900

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,25 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
-import { vercel } from "@/assets";
+import { Logo } from "@/assets";
 import Link from "next/link";
 
 const Header = () => {
-  const router = useRouter();
-  // const [searchValue, setSearchValue] = useState("");
-
-  // const handleSetValue = (e: { target: { value: any } }) => {
-  //   setSearchValue(e.target.value);
-  // };
   return (
-    // <div className="flex md:flex-row flex-col-reverse justify-between my-[25px] gap-6">
-    //   <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 bg-gray-200 rounded-[100px]">
-    //     <input
-    //       type="text"
-    //       placeholder="Search Project"
-    //       className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
-    //       onChange={(e: { target: { value: any } }) => handleSetValue(e)}
-    //     />
-    //   </div>
-    //   <ConnectButton
-    //     showBalance={false}
-    //     accountStatus={{ smallScreen: "address", largeScreen: "full" }}
-    //   />
-    // </div>
     <div>
       <nav className=" border-gray-200 px-4 lg:px-6 py-5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link href="./" className="flex items-center">
-            <img
-              src="https://avatars.githubusercontent.com/u/60056322?s=280&v=4"
+            <Image
+              src={Logo}
               className="mr-3 h-6 sm:h-9"
               alt="Logo"
+              width={40}
+              height={40}
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              ZORA
+              Eternal Commons
             </span>
           </Link>
           <div className="flex items-center lg:order-2">

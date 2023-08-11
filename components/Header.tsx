97a79c76e364e886e434d618aaef71831394c125
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-08-12 04:11:11 +0900

    fix typo

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <div>
       <nav className=" border-gray-200 px-4 lg:px-6 py-5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link href="./" className="flex items-center">
+          <Link href="/" className="flex items-center">
             <Image
               src={Logo}
               className="mr-3 h-6 sm:h-9"

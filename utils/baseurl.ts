ab2d11cb59ac6dd0cb929022b9a21d59201fda36
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-08-13 18:04:23 +0900

    typo

--- utils/baseurl.ts
+++ utils/baseurl.ts
@@ -1,6 +1,6 @@
 const BASE_URL = {
   11155111: "https://sepolia.easscan.org/attestation/view",
-  420: "https://optimism-goerli-bedrock.easscan.org/attention/view",
+  420: "https://optimism-goerli-bedrock.easscan.org/attestation/view",
   10: "https://optimism.easscan.org/attestation/view",
   84531: "https://base-goerli.easscan.org/attestation/view",
 };
